e60e9cfaf4f636138ab874c4de85cae111d36ccf
stillinbeta <web@stillinbeta.com>
2012-08-22 21:23:39 -0700

    A light proxy to cache geocoding requests

---
+++ application.py
@@ -0,0 +1,54 @@
+import os.path
+from urllib.parse import quote
+from urllib.request import urlopen
+from urllib.error import URLError
+import json
+
+
+import bottle
+from bottle_sqlite import SQLitePlugin
+
+dirname = os.path.dirname(os.path.abspath(__file__))
+
+bottle.install(SQLitePlugin(dbfile=dirname + '/db.sqlite3'))
+
+
+GOOGLE_API_URL = 'http://maps.googleapis.com/maps/api/geocode/json?sensor=true&address={city}'
+
+
+@bottle.route('/geocode/<city>')
+def geocode(city, db):
+    row = db.execute('SELECT lat, long FROM geocode WHERE city = ?',
+                     (city,)).fetchone()
+    if row:
+        return dict(row)
+
+    try:
+        response = urlopen(GOOGLE_API_URL.format(city=quote(city))).read()
+        decoded = json.loads(str(response, 'utf-8'))
+    except URLError as e:
+        bottle.abort(504, "Couldn't access Google API: " + str(e))
+    except ValueError as e:
+        bottle.abort(502, "Couldn't decode API response: " + str(e))
+
+    if not 'status' in decoded or decoded['status'] != 'OK':
+        bottle.abort(502, "Coludn't geocode request")
+
+    location = decoded['results'][0]['geometry']['location']
+    lat = location['lat']
+    lng = location['lng']
+
+    row = db.execute('INSERT INTO geocode (city, lat, long) VALUES (?, ?, ?)',
+                     (city, lat, lng,))
+    return {'lat': lat, 'long': lng}
+
+@bottle.route('/')
+def index():
+    return bottle.static_file('index.html', root=dirname)
+
+@bottle.route('/channel')
+def index():
+    return bottle.static_file('channel.html', root=dirname)
+
+if __name__ == '__main__':
+    bottle.run(host='localhost', port='8080')
